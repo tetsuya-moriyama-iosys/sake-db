181d1ba5e2344348cfc3b0a71e114602fc76af8a
unknown <t.moriyama@io-sys.co.jp>
2024-07-29 08:18:16 +0900

    test

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,15 +1,27 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/Index.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Index from '../views/Index.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'index',
-      component: Index
-    }
-  ]
-})
+      component: Index,
+    },
+  ],
+});
 
-export default router
+export default router;
