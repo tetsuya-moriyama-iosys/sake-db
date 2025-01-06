5e170b781cb2049eef61d42dba6160ad0a4069d4
unknown <t.moriyama@io-sys.co.jp>
2025-01-06 16:20:36 +0900

    途中保存

--- backend/router/oauth.go
+++ backend/router/oauth.go
@@ -6,6 +6,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 // ルートの設定
@@ -15,7 +16,7 @@ func oauthRoutes(r *gin.Engine, srv *handler.Server, handlers *handlers.Handlers
 		url, err := x.GenerateAuthURL()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+			return //TODO:エラーページに飛ばす
 		}
 		// JSONレスポンスでURLを返す
 		c.JSON(http.StatusOK, gin.H{
@@ -27,8 +28,25 @@ func oauthRoutes(r *gin.Engine, srv *handler.Server, handlers *handlers.Handlers
 		user, err := x.Login(handlers, c)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+			return //TODO:エラーページに飛ばす
 		}
+
+		//一時的なクッキーを作る
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     os.Getenv("SSO_AUTH_TOKEN_NAME"),
+			Value:    user.Token,
+			Path:     "/",
+			Domain:   "",
+			MaxAge:   60,
+			Secure:   false, //TODO:HTTPS対応が済んだらtrueにする
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+		})
+
+		// フロントエンドにリダイレクト
+		c.Redirect(http.StatusFound, "http://frontend-app-url/")
 		c.JSON(http.StatusOK, gin.H{"user": user})
+
+		//TODO:JWTトークンのみをクッキーに保存し、認証はストアからJWTトークンを用いた認証を改めて行う(userデータをクッキーに含めたくないため)
 	})
 }
