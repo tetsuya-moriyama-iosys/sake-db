5e270e73133cd962dcd82a178fb052c88c3556c2
unknown <t.moriyama@io-sys.co.jp>
2025-01-18 01:18:10 +0900

    HTTPS対応

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,13 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { constants } from 'crypto';
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
+
+dotenv.config(); // .env ファイルをロード
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +16,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    https: {
+      key: process.env.VITE_SSL_KEY_PATH,
+      cert: process.env.VITE_SSL_CERT_PATH,
+      secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1,
+    },
+    port: 5173,
   },
 });
