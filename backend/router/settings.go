9f68c8f6e60a85b234e46d61051eb3f2bd62474c
unknown <t.moriyama@io-sys.co.jp>
2025-01-31 20:32:52 +0900

    動いたっぽいところ

--- backend/router/settings.go
+++ backend/router/settings.go
@@ -16,28 +16,20 @@ func Router(srv *handler.Server, handlers *handlers.Handlers) *gin.Engine {
 	helper.LoadEnv()
 
 	// フロントエンドのURIを取得
-	//frontURI := getFrontURI()
+	frontURI := getFrontURI()
 
 	r := gin.Default()
 
 	// CORS設定
-	//r.Use(corsMiddleware(frontURI))
+	r.Use(corsMiddleware(frontURI))
 
 	/// ルート設定
 	configureRoutes(r, srv, handlers)
 
-	// 環境変数からパスを取得
-	certPath := os.Getenv("SSL_CERT_PATH")
-	keyPath := os.Getenv("SSL_KEY_PATH")
-
-	if certPath == "" || keyPath == "" {
-		log.Fatal("SSL_CERT_PATH or SSL_KEY_PATH is not set")
-	}
-
 	// HTTPSサーバーの起動
-	if err := r.RunTLS(":8080", certPath, keyPath); err != nil {
-		log.Fatalf("Failed to start server: %s", err)
-	}
+	//if err := r.RunTLS(":8080", certPath, keyPath); err != nil {
+	//	log.Fatalf("Failed to start server: %s", err)
+	//}
 
 	return r
 }
